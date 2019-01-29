f37c53e9a960982ec978c4da9dcbfbb0a29a0e3b
kaskichandrakant <chandk@thoughtworks.com>
2019-01-29 15:30:00 +0530

    new users are able to login

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template,flash,redirect,url_for,session,logging,request
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Article
 from flask_mysqldb import MySQL
-from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 import os
 
 
 app = Flask(__name__)
-app.config['MYSQL_HOST']=os.environ['MYSQL_HOST']
-app.config['MYSQL_USER']=os.environ['MYSQL_USER']
-app.config['MYSQL_PASSWORD']=os.environ['MYSQL_PASSWORD']
-app.config['MYSQL_DB']=os.environ['MYSQL_DB']
-app.config['MYSQL_CURSERCLASS']=os.environ['MYSQL_CURSERCLASS']
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+app.config['MYSQL_CURSORCLASS']=   os.environ['MYSQL_CURSORCLASS']
 
 mysql = MySQL(app)
 article = Article()
@@ -36,16 +36,18 @@ def articles():
 def get_article(id):
     return render_template('article.html', id=id)
 
+
 class RegisterForm(Form):
-    name = StringField('Name',[validators.Length(min=1,max=50)])
-    username = StringField('Username',[validators.Length(min=4,max=25)])
-    email = StringField('Email',[validators.Length(min=6,max=50)])
-    password = PasswordField('Password',[
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm',message='Password does not match')])
+        validators.EqualTo('confirm', message='Password does not match')])
     confirm = PasswordField('Confirm Password')
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -54,12 +56,44 @@ def register():
         username = form.username.data
         password = sha256_crypt.encrypt(str(form.password.data))
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name,email,username,password) VALUES(%s,%s,%s,%s)",(name,email,username,password))
+        cur.execute("INSERT INTO users(name,email,username,password) VALUES(%s,%s,%s,%s)",
+                    (name, email, username, password))
         mysql.connection.commit()
         cur.close()
-        flash('You are registerd','success')
-        return redirect(url_for('index'))
-    return render_template('register.html',form=form)
+        flash('You are registerd', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_hash = request.form['password']
+
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM users WHERE username = %s",[username])
+
+        if result > 0:
+            data = cur.fetchone()
+            password = data['password']
+            if sha256_crypt.verify(password_hash,password):
+                # app.logger.info('Passed')
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You are now logge in','success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid Password'
+                return render_template('login.html',error=error)
+        else:
+            error = 'Username not found'
+            return render_template('login.html',error=error)
+    return render_template('login.html')
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.secret_key = os.environ['SECRET_KEY']
